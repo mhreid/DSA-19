714c280a6d46f85fee6d726598266a4b0266547b
Micah <micahhelenreid@gmail.com>
2019-02-07 20:43:38 -0500

    Homework 3

--- datastructures/day03/src/Problems.java
+++ datastructures/day03/src/Problems.java
@@ -13,21 +13,75 @@ public class Problems {
     }
 
     public static List<Integer> removeKDigits(int[] A, int k) {
-        // TODO: your code here
-        // For now, return a List that's correct size, but contains only 0s
         List<Integer> l = new LinkedList<>();
-        for (int i = 0; i < A.length - k; i++) l.add(0);
+        for (int i = 0; i < A.length; i++) {
+            boolean add = true;
+            for (int j = 1; j <= k; j++) {
+                if ( i + j >= A.length || A[i] > A[i + j] ) {
+                    add = false;
+                    break;
+                }
+            }
+            if(add){
+                l.add(A[i]);
+            } else {
+                k--;
+            }
+        }
         return l;
     }
 
+
     public static boolean isPalindrome(Node n) {
-        // TODO: your code here
-        return false;
+        int size = 0;
+        Node curr = n;
+        while(curr != null){
+            size++;
+            curr = curr.next;
+        }
+
+        Node prev = null;
+        Node ahead = n;
+
+        for(int i = 0; i < size/2; i++){
+            curr = ahead;
+            ahead = curr.next;
+            curr.next = prev;
+            prev = curr;
+
+        }
+
+        Node curr2 = ahead;
+        if(size % 2 == 1){
+            curr2 = curr2.next;
+        }
+
+        for(int i = 0; i < size/2; i++){
+            if(curr.val != curr2.val){
+                return false;
+            }
+            curr = curr.next;
+            curr2 = curr2.next;
+        }
+        return true;
     }
 
     public static String infixToPostfix(String s) {
-        // TODO
-        return null;
+        List<Character> stack = new LinkedList<>();
+        String out = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '*' || s.charAt(i) ==  '/' || s.charAt(i) == '+' || s.charAt(i) == '-'){
+                stack.add(s.charAt(i));
+            } else if (s.charAt(i) == ')'){
+                out += (((LinkedList<Character>) stack).removeLast());
+                out += ' ';
+
+            } else if (s.charAt(i) != '(' && s.charAt(i) != ' '){
+                out += s.charAt(i);
+                out += ' ';
+            }
+        }
+        return out.substring(0, out.length() - 1);
     }
 
 }
