b0dba33aa12c6c24f40fc04f8682893ce6013a34
Micah <micahhelenreid@gmail.com>
2019-02-27 12:45:10 -0500

    Sorting day 03

--- sorting/day03/src/CountingSort.java
+++ sorting/day03/src/CountingSort.java
@@ -2,12 +2,28 @@ public class CountingSort {
 
     /**
      * Use counting sort to sort non-negative integer array A.
-     * Runtime: TODO
+     * Runtime: O(k + N)
      *
      * k: maximum element in array A
      */
     static void countingSort(int[] A) {
-        // TODO
+        int max = 0;
+        for(int i = 0; i < A.length; i++){
+            if(A[i] > max){
+                max = A[i];
+            }
+        }
+        int[] entries = new int[max + 1];
+        for(int i = 0; i < A.length; i++){
+            entries[A[i]]++;
+        }
+        int index = 0;
+        for(int i = 0; i < max + 1; i++){
+            for(int j = 0; j < entries[i]; j++){
+                A[index] = i;
+                index++;
+            }
+        }
     }
 
 }
--- sorting/day03/src/Problems.java
+++ sorting/day03/src/Problems.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Problems {
 
     static void sortNumsBetween100s(int[] A) {
-        // TODO
+        int[] counts = new int[201];
+        for(int i: A){
+            counts[i + 100]++;
+        }
+        int index = 0;
+        for(int i = 0; i < 201; i++){
+            for(int j = 0; j < counts[i]; j++){
+                A[index] = i - 100;
+                index++;
+            }
+        }
     }
 
     /**
@@ -19,14 +32,37 @@ public class Problems {
      * @param n The digit number (where 0 is the least significant digit)
      */
     static void countingSortByCharacter(String[] A, int n) {
-        // TODO
+        n = A[0].length() - n - 1;
+        int max = A[0].charAt(n) + 1;
+        for(String s: A){
+            if(s.charAt(n) > max){
+                max = s.charAt(n);
+            }
+        }
+        LinkedList<String>[] L = new LinkedList[max + 1];
+        for (int i = 0; i < max + 1; i++)
+            L[i] = new LinkedList<>();
+        String[] count = new String[max + 1];
+        for(String s: A){
+            L[s.charAt(n)].add(s);
+        }
+        int j = 0;
+        for (LinkedList<String> list : L) {
+            for(String entry: list){
+                A[j] = entry;
+                j++;
+
+            }
+        }
     }
 
     /**
      * @param stringLength The length of each of the strings in S
      */
     static void sortStrings(String[] S, int stringLength) {
-        // TODO
+        for (int i = 0; i < S[0].length(); i++){
+            countingSortByCharacter(S, i);
+        }
     }
 
     /**
