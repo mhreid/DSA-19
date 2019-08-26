dbbbd00f84869a9415a7f0277b3deb4a8b923cb8
Micah <micahhelenreid@gmail.com>
2019-08-26 12:25:01 -0400

    DP00

--- DP/dp00/src/DiceRollSum.java
+++ DP/dp00/src/DiceRollSum.java
@@ -1,10 +1,24 @@
 public class DiceRollSum {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(N)
+    // Space: O(N)
     public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+        int[] prev = new int[N + 1];
+        if(N == 0){
+            return 1;
+        }
+        prev[1] = 1;
+        for(int i = 2; i <= N; i++){
+                int count = 0;
+                if(i <= 6){
+                    count++;
+                }
+                int min = Math.max(1, i - 6);
+                for(int j = min; j <= i - 1 ; j++){
+                    count += prev[j];
+                }
+            prev[i] = count;
+        }
+        return prev[N];
     }
-
 }
--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,23 @@
 public class LongestIncreasingSubsequence {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(N^2)
+    // Space: O(N)
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        int[] DP = new int[A.length];
+        int max = 0;
+        if(DP.length == 0) return 0;
+        //if there is none there is 0, if there is 1 there is 1
+        DP[0] = 1;
+        for(int i = 1; i < A.length; i++){
+            int count = 1; //minimum length of subsequence
+            for(int j = 0; j < i; j++){
+                if(A[j] < A[i]){
+                    if(DP[j] + 1> count) count = DP[j] + 1;
+                }
+            }
+            DP[i] = count;
+            if(count > max) max = count;
+        }
+        return max;
     }
 }
